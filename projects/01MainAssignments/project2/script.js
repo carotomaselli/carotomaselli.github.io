2696f8c5959c6e0fee2fc48bc4d8a8303971a85f
carolinatomaselli <carolina.tomaselli@zhdk.ch>
2025-11-28 15:53:03 +0100

    Update script.js

--- projects/01MainAssignments/project2/script.js
+++ projects/01MainAssignments/project2/script.js
@@ -1,29 +1,61 @@
-let len = 10;
-let bg;
+
+
+let painting = false;
+let brushSize = 20;
+let erasing = false;
 
 function setup() {
-	createCanvas(windowWidth,windowHeight);
-	background(0);
-	frameRate(30);
-	rectMode(CENTER);
-	bg = color(200, 0, 58);
+  createCanvas(windowWidth, windowHeight);
+  background(255);
 }
 
-function  draw() {
-	background(bg);
-	noFill();
-	strokeWeight(5);
-	stroke(0,130, 140);
-	
-	ellipse(pmouseX, pmouseY, len, len)
-  //height - mouseY mirrors the object along y axis
-  	ellipse(pmouseX, height - pmouseY, len, len)
-	//increase the lenght by 1px every frame
-	len++;
-	
-	if(mouseIsPressed) {
-		len = 0;  
-		bg = color(189, 3, 33);
-	}
+function draw() {
+  
+  noStroke();
+  fill(0);
+  textAlign(CENTER, TOP);
+  textSize(24);
+  text("Draw something! If you want to erase, press the BACKSPACE key.", width/2, 10);
+
+  
+  if (painting) {
+    if (!erasing) {
+      // normales Zeichnen
+      stroke(0);
+      strokeWeight(brushSize);
+      line(pmouseX, pmouseY, mouseX, mouseY);
+    } else {
+      // Radieren durch Kopieren von benachbarten Pixeln
+      let w = brushSize * 2;
+      let h = brushSize * 2;
+      let sx = mouseX - w/2;
+      let sy = mouseY - h/2;
+      let dx = mouseX + random(-40, 40);
+      let dy = mouseY + random(-40, 40);
+      copy(sx, sy, w, h, dx, dy, w, h);
+      // wildes Gekritzel
+      stroke(random(255), random(255), random(255));
+      strokeWeight(random(1, 5));
+      line(mouseX, mouseY, mouseX + random(-30, 30), mouseY + random(-30, 30));
+    }
+  }
 }
 
+function mousePressed() {
+  painting = true;
+}
+
+function mouseReleased() {
+  painting = false;
+}
+
+function keyPressed() {
+  if (keyCode === BACKSPACE) {
+    erasing = true;
+    return false;
+  }
+
+  if (keyCode === ENTER || key === 'B') {
+    erasing = false;
+  }
+}
